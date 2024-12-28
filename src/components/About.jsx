27b0a41f317e72cc90dcf2a37cc346c403173d82
CiaranFlanagan <157981950+CiaranFlanagan@users.noreply.github.com>
2024-12-28 13:36:05 -0500

    Initial commit ... setup, about, navbar, profile

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import aboutImg from "../assets/lebronald.jpeg"
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h2 className="my-20 text-center text-4xl">
+            About
+            <span className="text-neutral-500"> Me</span> 
+        </h2>
+
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 lg:p-8">
+            <div className="flex items-center justify-center">
+                <img src={aboutImg} alt="img did not load..." />
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+
+export const PROFILE_CONTENT = `...`;
+
+export const ABOUT_TEXT = `...`;
+
+export const EXPERIENCES = [
+  {
+    year: "...",
+    role: "...",
+    company: "...",
+    description: `...`,
+    technologies: ["...", "...", "...", "..."],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "...",
+    image: "...",
+    description:
+      "...",
+    technologies: ["..."],
+  },
+];
+
+export const CONTACT = {
+  address: "",
+  phoneNo: " ",
+  email: "",
+};
