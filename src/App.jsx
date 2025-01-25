fd22c48fd11b4fd10214c5c2ee8732aacd01943f
CiaranFlanagan <157981950+CiaranFlanagan@users.noreply.github.com>
2025-01-25 13:12:09 -0500

    navbar stuff, smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ import BackToTopButton from "./components/BackToTopButton";
 
 const App = () => {
   return (
-    <div className="text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="overflow-x-hidden h-screen text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 min-h-screen flex flex-col">
+      <div className="fixed top-0 -z-10 w-full min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-      <div className="container mx-auto px-8 flex flex-col justify-between">
-        <Navbar />
+      <Navbar />
+      <div className="container mx-auto px-8 flex-grow flex flex-col justify-between pt-24">
         <section id="profile">
           <Profile />
         </section>
@@ -32,8 +32,8 @@ const App = () => {
         <section id="contact">
           <Contact />
         </section>
-        <BackToTopButton />
       </div>
+      <BackToTopButton />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
+  const handleScroll = (e, targetId) => {
+    e.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <nav className="mb-20 flex items-center justify-between py-6 px-4 md:px-8">
+    <nav className="fixed top-0 left-0 w-full bg-transparent backdrop-blur-md z-50 flex items-center justify-between py-6 px-4 md:px-8">
       <div className="flex flex-shrink-0 items-center">
         <span
           style={{ fontFamily: "Fredoka One, cursive" }}
@@ -13,32 +21,56 @@ const Navbar = () => {
       </div>
       <ul className="hidden md:flex space-x-4">
         <li>
-          <a href="#profile" className="hover:text-galwayMaroon-700">
+          <a
+            href="#profile"
+            onClick={(e) => handleScroll(e, "profile")}
+            className="hover:text-galwayMaroon-700"
+          >
             Profile
           </a>
         </li>
         <li>
-          <a href="#about" className="hover:text-galwayMaroon-700">
+          <a
+            href="#about"
+            onClick={(e) => handleScroll(e, "about")}
+            className="hover:text-galwayMaroon-700"
+          >
             About
           </a>
         </li>
         <li>
-          <a href="#technologies" className="hover:text-galwayMaroon-700">
+          <a
+            href="#technologies"
+            onClick={(e) => handleScroll(e, "technologies")}
+            className="hover:text-galwayMaroon-700"
+          >
             Technologies
           </a>
         </li>
         <li>
-          <a href="#experience" className="hover:text-galwayMaroon-700">
+          <a
+            href="#experience"
+            onClick={(e) => handleScroll(e, "experience")}
+            className="hover:text-galwayMaroon-700"
+          >
             Experience
           </a>
         </li>
         <li>
-          <a href="#projects" className="hover:text-galwayMaroon-700">
+          <a
+            href="#projects"
+            onClick={(e) => handleScroll(e, "projects")}
+            className="hover:text-galwayMaroon-700"
+          >
             Projects
           </a>
         </li>
         <li>
-          <a href="#contact" className="hover:text-galwayMaroon-700">
+          <a
+            href="#contact"
+            onClick={(e) => handleScroll(e, "contact")}
+            className="hover:text-galwayMaroon-700"
+          >
             Contact
           </a>
         </li>
